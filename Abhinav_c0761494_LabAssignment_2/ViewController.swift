7bc95977626e8aac844b84e134c285763bb33559
Abhinav Bhardwaj <abhinavbhardwaj034@gmail.com>
2020-01-19 14:17:56 -0500

    Initial Commit

---
+++ Abhinav_c0761494_LabAssignment_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Abhinav_c0761494_LabAssignment_2
+//
+//  Created by Abhinav Bhardwaj on 2020-01-19.
+//  Copyright © 2020 Abhinav Bhardwaj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
